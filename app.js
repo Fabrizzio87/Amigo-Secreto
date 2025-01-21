5de69807eae501d5629a92b55fef2ae54f20ebef
Fabrizzio87 <fabricio.h.villegas@gmail.com>
2025-01-20 23:48:03 -0300

    Proyecto amigo secreto terminado

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombre = [];
+let nuevoNombre = 0;
+
+
+function agregarAmigo() {
+    let nuevoNombre = document.getElementById("amigo").value;
+
+    if (nuevoNombre.trim() === "") {
+        alert("Por favor ingrese un nombre válido");
+        return;
+    }    
+
+    if (listaNombre.includes(nuevoNombre)) {
+        alert(`El nombre ${nuevoNombre} ya existe en la lista.`);
+        console.log(`El nombre ${nuevoNombre} ya existe en la lista.`);
+        limpiarCaja();   
+    } else {
+        listaNombre.push(nuevoNombre);
+        console.log(`Nombre agregado: ${nuevoNombre}`);
+        console.log(listaNombre);
+        // Crear un nuevo elemento <li>
+        let li = document.createElement("li");
+        li.textContent = nuevoNombre; // Asignar el nombre al contenido del <li>
+        
+        // Agregar el <li> al <ul>
+        document.getElementById("listaAmigos").appendChild(li);
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja(params) {
+    let valorCaja = document.querySelector("#amigo");
+    valorCaja.value = "";
+}
+
+function sortearAmigo() {
+    if (listaNombre.length === 0) {
+        alert("No hay amigos para sortear.");
+        console.log("No hay amigos para sortear.");
+        return;
+    }
+
+    // Seleccionar un nombre aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * listaNombre.length);
+    let amigoSorteado = listaNombre[indiceAleatorio];
+    console.log(amigoSorteado)
+    
+    
+    // Limpiar resultados anteriores
+    document.getElementById("resultado").innerHTML = ""; 
+    document.getElementById("listaAmigos").innerHTML = ""; 
+    
+
+    // Crear un nuevo elemento <li> para el resultado
+    let liamigoSecreto = document.createElement("li");
+    liamigoSecreto.textContent = `El amigo secreto sorteado es: ${amigoSorteado}`; // Asignar el nombre sorteado al contenido del <li>
+    
+    // Agregar el <li> al <ul> de resultados
+    //resultadoUl.appendChild(liamigoSecreto);
+    
+    // Mostrar el resultado en el DOM
+    //let resultadoUl = document.getElementById("resultado");
+
+    document.getElementById("resultado").appendChild(liamigoSecreto);
+}
+
+
+
